8d568bf35a9c4dda2a2c7f6f5222d1c9a0cf03a3
duvanceron <duvanceroon@hotmail.com>
2019-05-10 00:56:53 -0500

    morgan

--- mern-tasks/src/index.js
+++ mern-tasks/src/index.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const app=express();
 
-app.listen(3000,()=>{
-    console.log('server running');
-});
+//configuraciones
+app.set('port',process.env.PORT||3000)//tome el puerto de mi aplicación en el sistema operativo
+
+
+//Middlewares(funciones que se ejecuten antes de que lleguen a las rutas)
+
+
+app.listen(app.get('port'),()=>{
+    console.log(`servidor en el puerto ${app.get('port')}`);
+});//servidor escucha en el puerto 3000
