477ab464a986e3e9cc07eb05bd025acdb1410b65
duvanceron <duvanceroon@hotmail.com>
2019-05-11 17:44:01 -0500

    task

---
+++ mern-tasks/src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends Component{
+    render(){
+        return(
+            <h1>Welcome to the database</h1>
+        )
+
+    }
+}
+
+export default App;
